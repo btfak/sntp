5ebdb7c9a147535bdde7c6aa562b7c2c23659cb5
Brian Tobin <brian@meraki.com>
2019-10-29 18:10:49 -0700

    Specify a positive or negative day offset relative to current time

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 	"flag"
 	"github.com/briantobin/sntp/netapp"
 	"github.com/briantobin/sntp/netevent"
+	"github.com/briantobin/sntp/sntp"
 )
 
 func main() {
 	var port = flag.Int("p", 123, "NTP port")
+	var offset_days = flag.Int("o", 0, "Time offset (in days)")
 	flag.Parse()
+
+	sntp.Offset_days = int64(*offset_days)
 
 	var handler = netapp.GetHandler()
 	netevent.Reactor.ListenUdp(*port, handler)

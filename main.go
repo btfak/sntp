e8d988946fa890784fa5e830e9b576dbc986880f
Fabio Akio Takeuchi <fakiot@gmail.com>
2020-10-27 19:46:47 -0300

    Added custom function example on main.go

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
-//package main
-//about: btfak.com
-//create: 2013-9-25
-//update: 2016-08-22
+// package main
+// Modifications copyright (C) 2020 Fabio A. Takeuchi
 
 package main
 
 import (
-	"github.com/btfak/sntp/netapp"
-	"github.com/btfak/sntp/netevent"
+	"time"
+
+	"github.com/fakiot/sntp/netapp"
+	"github.com/fakiot/sntp/netevent"
 )
+
+type test struct{}
+
+// Sync method to return example datetime
+func (v *test) Sync() time.Time {
+	return time.Date(2005, 1, 1, 1, 1, 1, 0, time.Local)
+}
 
 func main() {
 	var handler = netapp.GetHandler()
+	// Uncomment this to see the example. Result datetime is 2005-01-01 01:01:01
+	// sntp.SetSyncFunc(&test{})
 	netevent.Reactor.ListenUdp(123, handler)
 	netevent.Reactor.Run()
 }
